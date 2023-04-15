d7bb1089aacdb3a1920a247e193398b67ada3b87
james.hsueh77 <james.hsueh@titansoft.com.sg>
2023-04-15 16:12:37 +0800

    feat: add reset in css and add some css design in home

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -1,13 +1,15 @@
 <template>
-<span>
+<span class="home-circle">
     Insightful wisdom Inspiring ideas
 </span>
-  <p>
-      Transforming wisdom
+  <p class="home-slogan">
+      Transforming
+      <span class="home-wisdom">wisdom</span>
+
       into reality
       through design.
   </p>
-  <img src="../assets/images/me-photo.png">
+  <img src="../assets/images/me-photo.png" class="home-photo">
 </template>
 
 <script>
