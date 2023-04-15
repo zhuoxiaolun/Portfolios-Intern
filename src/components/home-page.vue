5a4a5601b7ec039da8e7b8eed1baa9fdd9dfcc8d
james.hsueh77 <james.hsueh@titansoft.com.sg>
2023-04-15 23:05:59 +0800

    feat: add style for home page

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -1,27 +1,38 @@
 <template>
+  <div class="home">
     <div class="home-left">
-      <div class="home-circle">
-
-          <svg viewBox="0 0 100 100">
-            <path d="M 50 50 m -50 0 a 50 50 0 1 1 100 0 a 50 50 0 1 1 -100 0 z" id="circle" />
-            <text><textPath xlink:href="#circle" >Insightful&nbsp;wisdom&nbsp;Inspiring&nbsp; ideas &nbsp;</textPath></text>
-          </svg>
+      <div class="home-circlar">
+        <svg viewBox="0 0 100 100">
+          <path d="M 50 50 m -50 0 a 50 50 0 1 1 100 0 a 50 50 0 1 1 -100 0 z" id="home-circlar" />
+          <text stroke="#FE600F"><textPath xlink:href="#home-circlar">Insightful wisdom Inspiring ideas </textPath></text>
+        </svg>
       </div>
-        <img src="../assets/images/me-photo.png" class="home-photo">
+
+
+        <img src="../assets/images/me-photo.png" class="home-photo" >
     </div>
     <div class="home-right">
-      <p class="home-slogan">
-          Transforming
-          <span class="home-wisdom">wisdom</span>
-
-          into reality
-          through design.
-      </p>
+      <div class="home-slogan">
+        <p>Transforming <span class="home-wisdom">&nbsp;wisdom</span> </p>
+        <p>into reality</p>
+        <p>through design</p>
+      </div>
+      <img  src="../assets/images/home-continue.png" class="home-btn"/>
     </div>
+  </div>
 </template>
 
 <script>
+import {onMounted} from "vue";
+
 export default {
-    name: "HomePage"
+    name: "HomePage",
+    props:{
+    },
+    methods: {
+    }
 }
+
+onMounted( ()=>{
+});
 </script>
