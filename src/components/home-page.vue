819b7d8d0ed8503976412c2c9b5a081fbeb8e8e0
xiaolun <zhuoxl3729@gmail.com>
2023-04-15 17:16:56 +0800

    feat: circle affect to home-circle

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -1,15 +1,23 @@
 <template>
-<span class="home-circle">
-    Insightful wisdom Inspiring ideas
-</span>
-  <p class="home-slogan">
-      Transforming
-      <span class="home-wisdom">wisdom</span>
+    <div class="home-left">
+      <div class="home-circle">
 
-      into reality
-      through design.
-  </p>
-  <img src="../assets/images/me-photo.png" class="home-photo">
+          <svg viewBox="0 0 100 100">
+            <path d="M 50 50 m -50 0 a 50 50 0 1 1 100 0 a 50 50 0 1 1 -100 0 z" id="circle" />
+            <text><textPath xlink:href="#circle" >Insightful&nbsp;wisdom&nbsp;Inspiring&nbsp; ideas &nbsp;</textPath></text>
+          </svg>
+      </div>
+        <img src="../assets/images/me-photo.png" class="home-photo">
+    </div>
+    <div class="home-right">
+      <p class="home-slogan">
+          Transforming
+          <span class="home-wisdom">wisdom</span>
+
+          into reality
+          through design.
+      </p>
+    </div>
 </template>
 
 <script>
