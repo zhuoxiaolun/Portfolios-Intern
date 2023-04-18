73d4ef76865d0304311712179bf90984807a8d29
xiaolun <zhuoxl3729@gmail.com>
2023-04-19 00:11:46 +0800

    feat: add portfoliopage css

--- src/components/myPortfolios-page.vue
+++ src/components/myPortfolios-page.vue
@@ -12,20 +12,30 @@
                 <img  src="../assets/images/left/02.png" alt="02" class="left-02"/>
             </div>
             <div class="portfolio-left-work03">
-
+                <img  src="../assets/images/left/03.png" alt="03" class="left-03"/>
             </div>
-            <div class="portfolio-left-work04"></div>
+            <div class="portfolio-left-work04">
+                <img  src="../assets/images/left/04.png" alt="04" class="left-04"/>
+            </div>
         </div>
         <div class="portfolio-right-work">
-            <div class="portfolio-right-work05"></div>
-            <div class="portfolio-right-work06"></div>
-            <div class="portfolio-right-work07"></div>
-            <div class="portfolio-right-work08"></div>
+            <div class="portfolio-right-work05">
+                <img  src="../assets/images/right/05.png" alt="05"  class="right-05"/>
+            </div>
+            <div class="portfolio-right-work06">
+                <img  src="../assets/images/right/06.png" alt="06"  class="right-06"/>
+            </div>
+            <div class="portfolio-right-work07">
+                <img  src="../assets/images/right/07.png" alt="07"  class="right-07"/>
+            </div>
+            <div class="portfolio-right-work08">
+                <img  src="../assets/images/right/08.png" alt="08"  class="right-08"/>
+            </div>
         </div>
-
-
     </div>
-
+    <div class="portfolio-gohome">
+        <img  src="../assets/images/go-home.png " alt="home" class="go-home-btn"/>
+    </div>
 </div>
 </template>
 
