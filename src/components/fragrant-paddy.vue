8cef477c92c7e4befcb52b428d6630492946625e
xiaolun <zhuoxl3729@gmail.com>
2023-04-20 00:27:18 +0800

    feat: add  fragrant-page css

--- src/components/fragrant-paddy.vue
+++ src/components/fragrant-paddy.vue
@@ -1,42 +1,49 @@
 <template>
-<div class="fragrant-paddy">
-    <div class="fragrant-article">
-        <p class="fragrant-name-ch">
-            稻香忘憂遊
-        </p>
-        <p class="fragrant-name-en">
 
-            Fragrant Paddy Festival
-        </p>
-        <p class="fragrant-topic">
-            地方品牌再造<br>
-        </p>
-        <p class="fragrant-topic-name">
-            台中外埔忘憂谷食農體驗營
-        </p>
-        <p class="fragrant-inside-article">
-            我們發現忘憂谷雖然景色優美，適合家庭休閒假
-            日過來放鬆拍照，但是感覺少了展現當地特色的
-            活動，單純被大眾定義成一個拍照地點，但其實
-            外埔區擁有各種種植優良有機小農的農家，當地
-            卻沒有一個好的曝光機會給予他們，因此我們想
-            加強當地觀光和小農經濟為在地農業創造附加新
-            價值，藉此翻轉外埔，展現生機。
-        </p><br>
-        <p class="fragrant-inside-article">
-            於是我們透過舉辦馬拉松路跑活動於忘憂谷，並
-            且結合當地小農市集，讓家長帶著小孩可以在運
-            動的過程中欣賞風景，也能夠享受到當地有機農
-            作物，並且舉辦控窯活動，促進親子關係的同時
-            也能品嘗當地新鮮美食。
-        </p>
-    </div>
-    <div class="fragrant-work">
-        <div class="fragrant-photo">
-        <img  src="../assets/images/fragrant/01-photo.png" alt="01"  class="fragrant"/>
+    <div class="fragrant-all">
+        <div class="fragrant-delete">
+            <img src="../assets/images/fragrant/delete.png" alt="01" class="fragrant-delete-photo" />
         </div>
+        <div class="fragrant-paddy">
+            <div class="fragrant-article">
+                <p class="fragrant-name-ch">
+                    稻香忘憂遊
+                </p>
+                <p class="fragrant-name-en">
+
+                    Fragrant Paddy Festival
+                </p>
+                <p class="fragrant-topic">
+                    地方品牌再造<br>
+                </p>
+                <p class="fragrant-topic-name">
+                    台中外埔忘憂谷食農體驗營
+                </p>
+                <p class="fragrant-inside-article">
+                    我們發現忘憂谷雖然景色優美，適合家庭休閒假
+                    日過來放鬆拍照，但是感覺少了展現當地特色的
+                    活動，單純被大眾定義成一個拍照地點，但其實
+                    外埔區擁有各種種植優良有機小農的農家，當地
+                    卻沒有一個好的曝光機會給予他們，因此我們想
+                    加強當地觀光和小農經濟為在地農業創造附加新
+                    價值，藉此翻轉外埔，展現生機。
+                </p><br>
+                <p class="fragrant-inside-article">
+                    於是我們透過舉辦馬拉松路跑活動於忘憂谷，並
+                    且結合當地小農市集，讓家長帶著小孩可以在運
+                    動的過程中欣賞風景，也能夠享受到當地有機農
+                    作物，並且舉辦控窯活動，促進親子關係的同時
+                    也能品嘗當地新鮮美食。
+                </p>
+            </div>
+            <div class="fragrant-work">
+                <div class="fragrant-photo">
+                <img  src="../assets/images/fragrant/01-photo.png" alt="01"  class="fragrant" />
+                </div>
+            </div>
+        </div>
+
     </div>
-</div>
 </template>
 
 <script>
