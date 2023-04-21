b8256ab1cc7d5083a8bd7f46b9ecaf7a3d5e93e4
xiaolun <zhuoxl3729@gmail.com>
2023-04-21 19:48:52 +0800

    feat: fix portfolios-page overlay

--- src/components/Portfolios/myPortfolios-page.vue
+++ src/components/Portfolios/myPortfolios-page.vue
@@ -6,38 +6,82 @@
     <div class="portfolio-work">
         <div class="portfolio-left-work-container">
             <div class="portfolio-left-work portfolio-relay">
-                <img class="portfolio-image" src="../../assets/images/left/01.png" alt="01" />
-                <div class="overlay">test</div>
+                <img class="portfolio-image" src="../assets/images/left/01.png" alt="01" />
+                <div class="overlay">
+                    <div class="overlay-font">
+                        <p>品牌再造設計</p>
+                        <p class="border">台中外埔忘憂谷</p>
+                    </div>
+                </div>
             </div>
             <div class="portfolio-left-work portfolio-relay">
-                <img class="portfolio-image" src="../../assets/images/left/02.png" alt="02" />
-                <div class="overlay">test</div>
+                <img class="portfolio-image" src="../assets/images/left/02.png" alt="02" />
+                <div class="overlay">
+                    <div class="overlay-font">
+                        <p>個人網站設計</p>
+                        <p class="border">ABOUT ZhuoXL</p>
+
+                    </div>
+                </div>
             </div>
             <div class="portfolio-left-work portfolio-relay">
-                <img class="portfolio-image" src="../../assets/images/left/03.png" alt="03" />
-                <div class="overlay">test</div>
+                <img class="portfolio-image" src="../assets/images/left/03.png" alt="03" />
+                <div class="overlay">
+                    <div class="overlay-font">
+                        <p>平面海報設計</p>
+                        <p class="border">愛與和平</p>
+
+                    </div>
+                </div>
             </div>
             <div class="portfolio-left-work portfolio-relay">
-                <img class="portfolio-image" src="../../assets/images/left/04.png" alt="04" />
-                <div class="overlay">test</div>
+                <img class="portfolio-image" src="../assets/images/left/04.png" alt="04" />
+                <div class="overlay">
+                    <div class="overlay-font">
+                        <p>插畫創作</p>
+                        <p class="border">精靈之境</p>
+                    </div>
+               </div>
             </div>
         </div>
         <div class="portfolio-right-work-container">
             <div class="portfolio-right-work portfolio-relay">
-                <img class="portfolio-image" src="../../assets/images/right/05.png" alt="05"  />
-                <div class="overlay">test</div>
+                <img class="portfolio-image"  src="../assets/images/right/05.png" alt="05"  />
+                <div class="overlay">
+                    <div class="overlay-font">
+                        <p>品牌設計企劃</p>
+                        <p class="border">酗事繪畫工作室</p>
+                    </div>
+
+                </div>
             </div>
             <div class="portfolio-right-work portfolio-relay">
-                <img class="portfolio-image" src="../../assets/images/right/06.png" alt="06"  />
-                <div class="overlay">test</div>
+                <img class="portfolio-image" src="../assets/images/right/06.png" alt="06"  />
+                <div class="overlay">
+                    <div class="overlay-font">
+                        <p>平面海報設計</p>
+                        <p class="border">攻擊行動</p>
+                    </div>
+
+                </div>
             </div>
             <div class="portfolio-right-work portfolio-relay">
-                <img class="portfolio-image" src="../../assets/images/right/07.png" alt="07"  />
-                <div class="overlay">test</div>
+                <img class="portfolio-image" src="../assets/images/right/07.png" alt="07"  />
+                <div class="overlay">
+                    <div class="overlay-font">
+                        <p>平面海報設計</p>
+                        <p class="border">老屋磚</p>
+                    </div>
+                </div>
             </div>
             <div class="portfolio-right-work portfolio-relay">
-                <img class="portfolio-image" src="../../assets/images/right/08.png" alt="08" />
-                <div class="overlay">test</div>
+                <img class="portfolio-image" src="../assets/images/right/08.png" alt="08" />
+                <div class="overlay">
+                    <div class="overlay-font">
+                        <p>3D建模設計</p>
+                        <p class="border">BLENDER</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
